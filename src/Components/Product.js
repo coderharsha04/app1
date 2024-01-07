d2a9ab27be05e0ba0b8108a49c7941efac3894cb
coderharsha04 <harshkumar.bt19cse@pec.edu.in>
2024-01-07 18:21:53 +0530

    props

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,9 +1,6 @@
 import "./Product.css";
 
-function Product({ name, desc }) {
-  function handleClick() {
-    window.alert("hello");
-  }
+function Product({ name, desc, handleClick }) {
   return (
     <div className="product" onClick={handleClick}>
       <h1>{name}</h1>
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,14 +1,46 @@
 import Product from "./Product";
 import "./Product.css";
+import { useState } from "react";
 
 function Products() {
+  const [message, setMessage] = useState("");
+  function handleClick(val) {
+    if (val === "1") {
+      setMessage("First Product clicked");
+    }
+    else if (val === "2") {
+      setMessage("Second Product clicked");
+    }
+    else{
+      setMessage("Third Product clicked");
+    }
+  }
   return (
-    <div className="products">
-      <Product name="Bajaj Kettle" desc="Thik hi chalti hai" />
-      <Product name="Bajaj Tea" desc="Thik hi chalti hogi" />
-      <Product name="Bajaj Tea" desc="Thik hi chalti hogi" />
-      <Product name="Bajaj Tea" desc="Thik hi chalti hogi" />
-      <Product name="Bajaj Tea" desc="Thik hi chalti hogi" />
+    <div>
+      <div className="products">
+        <Product
+          name="Bajaj Kettle"
+          desc="Thik hi chalti hai"
+          handleClick={() => {
+            handleClick("1");
+          }}
+        />
+        <Product
+          name="Bajaj Tea"
+          desc="Thik hi chalti hogi"
+          handleClick={() => {
+            handleClick("2");
+          }}
+        />
+        <Product
+          name="Bajaj Roaster"
+          desc="Thik hi chalti hogi"
+          handleClick={() => {
+            handleClick("3");
+          }}
+        />
+      </div>
+      <p>{message}</p>
     </div>
   );
 }
